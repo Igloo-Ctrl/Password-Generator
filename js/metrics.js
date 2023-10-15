e5264a702389d02775c8bd6aa1267d5f3f1f65cb
ryanjones <ryanleejones@outlook.com>
2023-10-15 11:37:28 +0100

    Fixed github logo

--- js/metrics.js
+++ js/metrics.js
@@ -1,3 +1,12 @@
 function metrics() {
-    console.log("test");
+    const key = "visitOrRefreshCount"
+    if (localStorage.getItem(key)) {
+        let currentCount = parseInt(localStorage.getItem(key));
+        localStorage.setItem(key, (currentCount + 1).toString())
+    }
+    else {
+        localStorage.setItem(key, 1);
+    }
 }
+
+metrics();
